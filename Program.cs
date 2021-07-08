bcf82b04890954c1d8f5db8c62584a99eec8d5c3
yoanpetrov02 <87146784+yoanpetrov02@users.noreply.github.com>
2021-07-08 21:27:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            int m = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                m++;
+                array[i] = m;
+                Console.WriteLine(array[i]);
+            }
+            foreach (int item in array)
+            {
+                array[item] = 1;
+                Console.WriteLine(array[item]);
+            }
+        }
+    }
+}
